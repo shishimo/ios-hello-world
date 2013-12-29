a7c7df75487bc3c133a2a4dd22406286cdc07010
shishimo <my_blue_wish@yahoo.co.jp>
2013-12-29 15:20:10 +0900

    Hello World

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Shimoda Shinichiro on 2013/12/29.
+//  Copyright (c) 2013年 Shimoda Shinichiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Shimoda Shinichiro on 2013/12/29.
+//  Copyright (c) 2013年 Shimoda Shinichiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
